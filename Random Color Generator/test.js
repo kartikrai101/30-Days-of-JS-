4ddf83e722448d40677662092b9416d87e1fe396
Kartik <kitrakiar73@gmail.com>
2021-12-07 15:48:55 +0530

    initial commit

--- Random Color Generator/test.js
+++ Random Color Generator/test.js
@@ -1,9 +1,1 @@
-function generator(){
-   return Math.floor(Math.random()*256);
-}
 
-setInterval(()=>{
-  const color = `rgb(${generator()}, ${generator()}, ${generator()})`;
-  console.log(color);
-  document.querySelector('#test').style.backgroundColor= color;
-}, 1000);
